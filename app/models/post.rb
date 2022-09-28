48206199b99747b2879b5b1fd54c4c5ae5caf3a4
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-09-28 16:16:10 +0300

    commit tree 82ce1fa22a979b705e7f1f7641ee5be07eba7542

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ["Fiction","Non-Fiction"]
+
+    #custom validation
+    def title_check 
+        unless :title.include?("Won't Believe") || :title.include?("Secret") || :title.include?("Top [number]") || :title.include?("Guess")
+            erros.add(:title, " True Facts")
+        end
+    end
 end
